9096f47a3cf504ab6e8ed6f034603217bef390ac
sachin jat <53407215+sachinjat2802@users.noreply.github.com>
2023-06-26 22:39:18 +0530

    pattern programs day 2

---
+++ invertedTriangleStarPattern.java
@@ -0,0 +1,18 @@
+/**
+ * The NumberPattern class prints a square pattern of numbers from 1 to 4.
+ */
+class InvertedTriangleStarPattern {
+    public static void main(String[] args) {
+        int raw = 5;
+        int column =5;
+        String s ="";
+        for (int i = 1; i <= raw; i++) {
+            for (int j =1; j<= column; j++) {
+                if(i<j)
+                s +="*";
+            }
+            s+="\n";
+        }
+        System.out.println(s);
+   }
+}
